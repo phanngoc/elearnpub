e22c2f50987a5ef1d0906b7598cfd4ba55cb8d4e
Phan Ngoc <lequidon.1993@gmail.com>
2016-10-02 21:14:55 +0700

    .gitignore is working

--- vendor/maxhoffmann/parsedown-laravel/src/MaxHoffmann/Parsedown/ParsedownFacade.php
+++
@@ -1,9 +0,0 @@
-<?php namespace MaxHoffmann\Parsedown;
-
-use Illuminate\Support\Facades\Facade;
-
-class ParsedownFacade extends Facade {
-
-    protected static function getFacadeAccessor() { return 'parsedown'; }
-
-}
